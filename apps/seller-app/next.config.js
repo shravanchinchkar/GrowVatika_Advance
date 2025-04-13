efdfed093a8b3b2aaf8070c537442521308c18b1
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-04-13 12:36:30 +0530

    updated the next.config.js in seller-app

--- apps/seller-app/next.config.js
+++ apps/seller-app/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  reactStrictMode: true,
   output: 'standalone',
+  // Disable static optimization
+  experimental: {
+    // For Next.js 15, use this instead of the deprecated appDir
+    staticPageGenerationTimeout: 0
+  }
 };
 
 export default nextConfig;

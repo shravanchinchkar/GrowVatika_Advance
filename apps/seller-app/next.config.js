a538cdd498bf7da185c675424f7673bd3d73d1ed
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-04-13 12:31:34 +0530

    updated the custom error pages

--- apps/seller-app/next.config.js
+++ apps/seller-app/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // If using the app directory
-  experimental: {
-    appDir: true,
-  },
-  // Set this to false if you're having issues with context
+  
   output: 'standalone',
 };
 

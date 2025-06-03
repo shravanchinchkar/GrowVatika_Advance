dedb7c26001b5d4fb6e2c046d36ffe22985d4596
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-06-03 17:18:42 +0530

    update the seller landing page

--- apps/seller-app/components/seller-header-section.tsx
+++ apps/seller-app/components/seller-header-section.tsx
@@ -1,13 +1,11 @@
 import { SellerNavbar } from "./nav-bar";
 import Image from "next/image";
-
 export const SellerHeaderSection = () => {
   return (
     <div className="w-[82.1875rem] h-[20rem] border-[2px] mt-[1rem] rounded-[2.5rem] border-[#12352433]  pt-[1rem] flex flex-col justify-start gap-[1rem] shadow-custom">
       <SellerNavbar />
 
       <div className="flex h-[70%]">
-
         <div className="w-[55rem] h-[9rem] flex justify-center items-center bg-custom-gradient font-[Unbounded] rounded-r-[6.25rem]">
           <div className="w-[46.6875rem] flex flex-col text-[3rem] h-max leading-[50px] text-start font-bold text-[#fff]">
             <span>Welcome to</span>

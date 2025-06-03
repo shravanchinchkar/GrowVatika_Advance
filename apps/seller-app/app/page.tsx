2bfa6aaa205ca472a34e68e07082ba46edae38e0
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-06-03 17:52:26 +0530

    remove the comments

--- apps/seller-app/app/page.tsx
+++ apps/seller-app/app/page.tsx
@@ -3,7 +3,6 @@ import { SellerHeroSection } from "../components/seller-hero-section";
 
 export default function Home() {
   return (
-    // bg-[#FFF6F4]
     <div className="h-screen text-2xl bg-[#FFF6F4] flex flex-col items-center justify-between gap-[2.4rem] font-[Poppins] border-[2px]">
       <SellerHeaderSection/>
       <SellerHeroSection/>

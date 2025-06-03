cf91219cfc835db0b8438938dcf300e61fefe803
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-06-03 10:43:11 +0530

    fixing the seller landing page issue

--- apps/seller-app/app/page.tsx
+++ apps/seller-app/app/page.tsx
@@ -3,7 +3,8 @@ import { SellerHeroSection } from "../components/seller-hero-section";
 
 export default function Home() {
   return (
-    <div className="text-2xl h-[100vh] bg-[#FFF6F4] flex items-center flex-col gap-[2.4rem] font-[Poppins]">
+    // bg-[#FFF6F4]
+    <div className="text-2xl h-[100vh] bg-black flex items-center flex-col gap-[2.4rem] font-[Poppins]">
       <SellerHeaderSection/>
       <SellerHeroSection/>
     </div>

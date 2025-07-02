5518f61a6f4ed48ee906b0b5d59f678678af5b2b
akshaykalbor7030 <akshaykalbor7030@gmail.com>
2025-07-02 16:39:37 +0530

    updated mobile view of explore section

--- apps/user-app/components/explore-card.tsx
+++ apps/user-app/components/explore-card.tsx
@@ -16,7 +16,7 @@ type ExploreCardProps = {
 };
 
 export const ExploreCard: React.FC<ExploreCardProps> = ({ cardData }) => {
-  const router=useRouter();
+  const router = useRouter();
 
   const handleNavigation = () => {
     router.push("/explore")
@@ -24,12 +24,12 @@ export const ExploreCard: React.FC<ExploreCardProps> = ({ cardData }) => {
   return (
     <div
       key={cardData.id}
-      className="border-red-500 group border-[2px] relative flex justify-self-center new-sm:w-[9rem] new-sm:h-[8rem] new-sm-1:w-[17rem] new-sm-1:h-[13rem] lg:w-[17rem] lg:h-[13rem] xl:w-[19.6875rem] xl:h-[16.75rem] new-sm:rounded-[0.99rem] new-sm-1:rounded-[28px] overflow-hidden shadow-explore-custom cursor-pointer"
+      className="group border-[2px] relative flex new-sm:w-[9rem] new-sm:h-[8rem] lg:w-[17rem] lg:h-[13em] xl:w-[19.6875rem] xl:h-[16.75rem] rounded-[28px] overflow-hidden shadow-explore-custom cursor-pointer"
     >
       {/* Following div appear by-default */}
       <div className="absolute inset-0 grid grid-cols-2 transform transition-transform duration-300 group-hover:-translate-y-full">
-        <div className="bg-white lg:p-[0.5rem] xl:p-[1rem] flex flex-col justify-between">
-          <div className="uppercase text-[16px] font-medium">
+        <div className="bg-white new-sm:p-[0.6rem] new-sm-1:p-[0.6rem] lg:p-[0.5rem] xl:p-[1rem] flex flex-col justify-between">
+          <div className="uppercase new-sm:text-[0.6rem] new-sm-1:text-[16px] font-medium">
             {cardData.title}
           </div>
           <div>
@@ -37,7 +37,8 @@ export const ExploreCard: React.FC<ExploreCardProps> = ({ cardData }) => {
           </div>
         </div>
 
-        <div className="lg:w-[8rem] xl:w-[9rem] new-xl:w-[10.0452rem]  lg:h-[13rem] xl:h-[16.7419rem] relative">
+        <div
+          className="lg:w-[8rem] xl:w-[9rem] new-xl:w-[10.0452rem]  lg:h-[13rem] xl:h-[16.7419rem] relative">
           <Image
             className="object-cover"
             src={cardData.image}
@@ -50,17 +51,17 @@ export const ExploreCard: React.FC<ExploreCardProps> = ({ cardData }) => {
       </div>
 
       {/* Following div appears on hover */}
-      <div className="transform transition-transform duration-300 translate-y-full group-hover:translate-y-0 font-[Poppins] bg-[#123524B8] w-[100%] flex flex-col items-start lg:gap-0 xl:gap-[0.2rem]">
-        <div className="px-[2rem] lg:py-[0.5rem] xl:py-[1rem] text-[1.22688rem] font-semibold text-white">
+      <div className="transform transition-transform duration-300 translate-y-full group-hover:translate-y-0 font-[Poppins] bg-[#123524B8] w-[100%] flex flex-col items-start  lg:gap-0 xl:gap-[0.2rem]">
+        <div className="new-sm:h-[2rem] new-sm-1:h-[4rem] new-sm:px-[1.2rem] new-sm-1:px-[2rem] new-sm:py-[0.5rem] new-sm-1:py-[0.5rem] lg:py-[0.5rem] xl:py-[1rem] new-sm:text-[0.8rem] new-sm-1:text-[1.22688rem] font-semibold text-white">
           {cardData.title}
         </div>
 
-        <div className="text-[#CBD0D3] text-[1rem] font-normal w-[100%] flex flex-col">
+        <div className="text-[#CBD0D3] new-sm:text-[0.7rem] new-sm-1:text-[1rem] font-normal w-[100%] flex flex-col">
           {cardData.onHover.map((item: string) => {
             return (
               <div
                 key={uuidv4()}
-                className="w-[100%] lg:h-[30px] xl:h-[40px] flex items-center pl-[2rem] hover:bg-[#123524]  hover:drop-shadow-lg"
+                className="w-[100%] new-sm:h-[16px] new-sm-1:h-[40px] lg:h-[40px] xl:h-[40px] flex items-center new-sm:pl-[1.2rem] new-sm-1:pl-[2rem] hover:bg-[#123524] hover:drop-shadow-lg"
               >
                 {item}
               </div>
@@ -68,7 +69,7 @@ export const ExploreCard: React.FC<ExploreCardProps> = ({ cardData }) => {
           })}
         </div>
 
-        <div className="ml-[1.5rem] lg:mt-[1rem]">
+        <div className="new-sm:ml-[1.2rem] new-sm-1:ml-[1.5rem] new-sm:mt-[0.25rem] new-sm-1:mt-[0.5rem] lg:mt-[0.5rem]">
           <SiteButton buttonName={"Explore"} onClick={handleNavigation} />
         </div>
       </div>
--- apps/user-app/components/explore-section.tsx
+++ apps/user-app/components/explore-section.tsx
@@ -1,4 +1,4 @@
-import {ExploreCard} from "./explore-card";
+import { ExploreCard } from "./explore-card";
 import { ExploreMoreSectionData } from "../data/exploreMoreSectionData";
 
 export const ExploreSection = () => {
@@ -8,7 +8,7 @@ export const ExploreSection = () => {
 
       <div className="flex flex-col justify-center items-center new-sm:mt-0 sm:mt-1 mt-[1rem] font-[Poppins]">
 
-        <div className="new-sm:text-[1rem]  sm:text-[1.3rem] lg:text-[1.5rem] xl:text-[2.25rem] font-medium uppercase new-sm:w-[22rem] sm:w-max new-sm:h-[1.5rem] sm:h-[2rem] lg:h-[3.375rem] text-center text-[#123524] mt-[1rem]">
+        <div className="new-sm:text-[1.1rem]  sm:text-[1.3rem] lg:text-[1.5rem] xl:text-[2.25rem] font-medium uppercase new-sm:w-[22rem] sm:w-max new-sm:h-[1.5rem] sm:h-[2rem] lg:h-[3.375rem] text-center text-[#123524] mt-[1rem]">
           Explore <span className="font-bold ">a World of Green</span> – Plants,
           Tools & Essentials!
         </div>
@@ -22,8 +22,8 @@ export const ExploreSection = () => {
       </div>
 
       <div className="new-sm:mx-[2rem] new-sm:pt-[2.5rem] new-sm-1:px-[5rem] new-sm-1:py-[3rem] new-sm:grid new-sm:grid-cols-2 new-sm-1:flex gap-[1.5rem]">
-        {ExploreMoreSectionData.map((item)=>{
-          return <ExploreCard key={item.id} cardData={item}/>
+        {ExploreMoreSectionData.map((item) => {
+          return <ExploreCard key={item.id} cardData={item} />
         })}
       </div>
 

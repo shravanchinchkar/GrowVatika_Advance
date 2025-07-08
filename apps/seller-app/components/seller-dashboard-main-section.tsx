e8fc3a1bdcfc7d83975410cd510e6c4490267f31
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-07-08 18:02:59 +0530

    working on the rerendering of the component

--- apps/seller-app/components/seller-dashboard-main-section.tsx
+++ apps/seller-app/components/seller-dashboard-main-section.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import toast from "react-hot-toast";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useSellerDataStore } from "@repo/shared-store";
 import { toastStyle } from "@repo/shared/utilfunctions";
@@ -12,13 +12,15 @@ import { SellerDashboardProfileSection } from "./seller-dashboard-profile-sectio
 import { SellerDashboardAddProductSection } from "./seller-dashboard-add-product-section";
 import { SellerDashboardProductManagementSection } from "./seller-dashboard-product-management-section";
 
-export const SellerDashboardMainSection = () => {
+export const SellerDashboardMainSection = memo(() => {
   const searchParams = useSearchParams();
   const sellerId = searchParams?.get("id") || "";
   const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   // Following is the Zustand state management code for sellerData
-  const updateSellerData = useSellerDataStore((state) => state.updateSellerData);
+  const updateSellerData = useSellerDataStore(
+    (state) => state.updateSellerData
+  );
 
   // Following is the Zustand state management code for seller Product data
   const updateSellerProductData = useSellerProductDataStore(
@@ -26,7 +28,7 @@ export const SellerDashboardMainSection = () => {
   );
 
   useEffect(() => {
-    console.log("useEffect")
+    console.log("useEffect");
     setIsDataLoaded(false);
     const fetchData = async () => {
       try {
@@ -42,7 +44,7 @@ export const SellerDashboardMainSection = () => {
           nurseryName: res.data.sellerData.nurseryName,
           specialities: res.data.sellerData.specialities,
           businesshours: res.data.sellerData.business_hours,
-          profilePictureURL:res.data.sellerData.profilePictureURL
+          profilePictureURL: res.data.sellerData.profilePictureURL,
         });
         updateSellerProductData(res.data.sellerProductData);
         setIsDataLoaded(true);
@@ -71,4 +73,4 @@ export const SellerDashboardMainSection = () => {
       <SellerDashboardAddProductSection />
     </div>
   );
-};
+});

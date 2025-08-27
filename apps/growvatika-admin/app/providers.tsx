d11f9aac4d6ef4bcb63ba6243e7863213be8f109
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-08-27 11:11:49 +0530

    fix the admin-app deployment issue

--- apps/growvatika-admin/app/providers.tsx
+++ apps/growvatika-admin/app/providers.tsx
@@ -2,15 +2,12 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
-import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <NuqsAdapter>
-        <Toaster />
-        {children}
-      </NuqsAdapter>
+      <Toaster />
+      {children}
     </SessionProvider>
   );
 }

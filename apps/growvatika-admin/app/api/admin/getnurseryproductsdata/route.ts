fffbef5c320a08e142b45882e197cbaabdca21dd
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-08-28 20:08:22 +0530

    fix the admin-app deployment issue

--- apps/growvatika-admin/app/api/admin/getnurseryproductsdata/route.ts
+++ apps/growvatika-admin/app/api/admin/getnurseryproductsdata/route.ts
@@ -14,109 +14,108 @@ export async function GET(req: NextRequest) {
         },
         { status: 401 }
       );
-    } else {
-      const { searchParams } = new URL(req.url);
-      const nurseryId = searchParams?.get("nurseryId") || "";
-      const productCategory = searchParams?.get("category") || "";
+    }
+    const { searchParams } = new URL(req.url);
+    const nurseryId = searchParams?.get("nurseryId") || "";
+    const productCategory = searchParams?.get("category") || "";
 
-      if (nurseryId === "" || productCategory === "") {
-        return NextResponse.json(
-          {
-            success: false,
-            error: "Invalid Input",
-          },
-          { status: 400 }
-        );
-      }
-      // First, check if the nursery exists
-      const existingNursery = await client.seller.findUnique({
+    if (nurseryId === "" || productCategory === "") {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "Invalid Input",
+        },
+        { status: 400 }
+      );
+    }
+    // First, check if the nursery exists
+    const existingNursery = await client.seller.findUnique({
+      where: {
+        id: nurseryId,
+      },
+    });
+    if (!existingNursery) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: `Nursery with ID ${nurseryId} does not exists!`,
+        },
+        { status: 404 }
+      );
+    }
+
+    // Execute the following block if productCategory is All
+    if (productCategory === "All") {
+      const productData = await client.product.findMany({
         where: {
-          id: nurseryId,
+          sellerId: existingNursery.id,
+        },
+        select: {
+          imageURL: true,
+          name: true,
+          price: true,
+          compareAt: true,
+          productQuantity: true,
         },
       });
-      if (!existingNursery) {
+      if (productData.length === 0) {
         return NextResponse.json(
           {
             success: false,
-            error: `Nursery with ID ${nurseryId} does not exists!`,
+            error: `${existingNursery.nurseryName} have not published any products yet!`,
+            nurseryName: existingNursery.nurseryName,
           },
           { status: 404 }
         );
       }
-
-      // Execute the following block if productCategory is All
-      if (productCategory === "All") {
-        const productData = await client.product.findMany({
-          where: {
-            sellerId: existingNursery.id,
-          },
-          select: {
-            imageURL: true,
-            name: true,
-            price: true,
-            compareAt: true,
-            productQuantity: true,
-          },
-        });
-        if (productData.length === 0) {
-          return NextResponse.json(
-            {
-              success: false,
-              error: `${existingNursery.nurseryName} have not published any products yet!`,
-              nurseryName: existingNursery.nurseryName,
-            },
-            { status: 404 }
-          );
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Product data found!",
+          productData,
+          nurseryName: existingNursery.nurseryName,
+        },
+        {
+          status: 200,
         }
+      );
+    }
+    // Execute the following block if productCategory is other than all
+    if (productCategory !== "All") {
+      const productData = await client.product.findMany({
+        where: {
+          sellerId: existingNursery.id,
+          category: productCategory,
+        },
+        select: {
+          imageURL: true,
+          name: true,
+          price: true,
+          compareAt: true,
+          productQuantity: true,
+        },
+      });
+      if (productData.length === 0) {
         return NextResponse.json(
           {
-            success: true,
-            message: "Product data found!",
-            productData,
+            success: false,
+            error: `${existingNursery.nurseryName} have not published any products of category ${productCategory}!`,
             nurseryName: existingNursery.nurseryName,
           },
-          {
-            status: 200,
-          }
+          { status: 404 }
         );
       }
-      // Execute the following block if productCategory is other than all
-      if (productCategory !== "All") {
-        const productData = await client.product.findMany({
-          where: {
-            sellerId: existingNursery.id,
-            category: productCategory,
-          },
-          select: {
-            imageURL: true,
-            name: true,
-            price: true,
-            compareAt: true,
-            productQuantity: true,
-          },
-        });
-        if (productData.length === 0) {
-          return NextResponse.json(
-            {
-              success: false,
-              error: `${existingNursery.nurseryName} have not published any products of category ${productCategory}!`,
-              nurseryName: existingNursery.nurseryName,
-            },
-            { status: 404 }
-          );
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Product data found!",
+          productData,
+          nurseryName: existingNursery.nurseryName,
+        },
+        {
+          status: 200,
         }
-        return NextResponse.json(
-          {
-            success: true,
-            message: "Product data found!",
-            productData,
-            nurseryName: existingNursery.nurseryName,
-          },
-          {
-            status: 200,
-          }
-        );
-      }
+      );
     }
   } catch (error) {
     console.error("Error while getting product data", error);
